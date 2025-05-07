7280965bee4907d570fce55cdbc11b662beec1da
KYoshi-02 <yoshi.k.0202.h.02@gmail.com>
2025-05-07 19:13:26 +0900

    user.rb、マイグレーションファイルに追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  belongs_to :company
+  has_many :knowhows
+  has_many :answers
+
+  enum role: { general: 0, admin: 1 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :nick_name, presence: true, length: { maximum: 20 }
+
+  VALID_PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,20}\z/
+  validates :password, format: {
+    with: VALID_PASSWORD_REGEX,
+    message: 'は半角英数字6文字以上20文字以内で入力してください'
+  }, if: :password_required?
 end
--- db/migrate/20250506081826_devise_create_users.rb
+++ db/migrate/20250506081826_devise_create_users.rb
@@ -7,6 +7,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.string :name, null: false
+      t.string :nick_name, null: false
+      t.references :company, null: false, foreign_key: true
+      t.integer :role, null: false, default: 0
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
