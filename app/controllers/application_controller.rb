e73755a50df34eb4107abf05bd0233d01d1cb2a8
KYoshi-02 <yoshi.k.0202.h.02@gmail.com>
2025-05-09 19:21:27 +0900

    ログアウト機能導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
     devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
   end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    root_path
+  end
 end
